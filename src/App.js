836bdf8caed586e33303dce0fd9e7b5dd257a625
Tolunay <tolunay.tzcn7@gmail.com>
2021-11-19 13:04:40 +0300

    Add eslint,prettier and husky

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div>
-      <h1>Todo app!</h1>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div>
+			<h1>Todo App!</h1>
+		</div>
+	);
+};
 
 export default App;
