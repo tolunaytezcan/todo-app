084150d104c8f86ec534f6cf5ad7def4e4cd7ddf
Tolunay <tolunay.tzcn7@gmail.com>
2021-11-19 23:10:56 +0300

    Update text component

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyledText } from './style';
 
-export const Text = ({ value, color }) => <StyledText color={color}>{value}</StyledText>;
+export const Text = ({ value, color, style }) => (
+	<StyledText color={color} style={style}>
+		{value}
+	</StyledText>
+);
 
 Text.propTypes = {
 	value: PropTypes.string.isRequired,
 	color: PropTypes.oneOf(['primary', 'secondary', 'gray', 'white', 'black', 'red']),
+	style: PropTypes.object,
 };
